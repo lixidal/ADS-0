0780e051d113b45268b9b212a5d3b4083039c97b
Lix <zelenovaaleksandra2@gmail.com>
2025-03-02 23:18:03 +0300

    Update alg lll.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,15 @@
-// Copyright 2021 NNTU-CS
+#include <iostream>
 
 int gcd(int a, int b) {
+  while (b != 0) {
+    int temp = b;
+    b = a % b;
+    a = temp;
+  }
+  return a;
+}
 
+int main() {
+  std::cout << gcd(10, 15) << std::endl;
+  return 0;
 }
